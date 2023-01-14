9a7e5dd90f1fea304fedffd266afe32f1b4dfe11
River Fisher <riverfisher@Rivers-MBP-2.lan>
2023-01-14 16:24:07 -0500

    Episode 7: Make a Route and Link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<title>My Blog!</title>
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+<body>
+    <article>
+        <h1><a href="/post">My Fist Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat vitae dui mollis interdum. Aliquam erat volutpat. Vivamus a interdum elit. Nullam interdum lorem diam, suscipit tempor arcu ullamcorper a. Donec vestibulum, lectus nec vehicula scelerisque, augue quam placerat ligula, nec interdum risus lacus in tortor. Suspendisse enim turpis, pellentesque vitae massa a, mattis placerat leo. Pellentesque fringilla mattis tristique. Cras id convallis ante, a tempor eros. Nullam at semper ante. Praesent eros lacus, consequat sit amet dui nec, elementum efficitur justo. In hac habitasse platea dictumst. In aliquet sapien quis risus venenatis convallis. </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat vitae dui mollis interdum. Aliquam erat volutpat. Vivamus a interdum elit. Nullam interdum lorem diam, suscipit tempor arcu ullamcorper a. Donec vestibulum, lectus nec vehicula scelerisque, augue quam placerat ligula, nec interdum risus lacus in tortor. Suspendisse enim turpis, pellentesque vitae massa a, mattis placerat leo. Pellentesque fringilla mattis tristique. Cras id convallis ante, a tempor eros. Nullam at semper ante. Praesent eros lacus, consequat sit amet dui nec, elementum efficitur justo. In hac habitasse platea dictumst. In aliquet sapien quis risus venenatis convallis. </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat vitae dui mollis interdum. Aliquam erat volutpat. Vivamus a interdum elit. Nullam interdum lorem diam, suscipit tempor arcu ullamcorper a. Donec vestibulum, lectus nec vehicula scelerisque, augue quam placerat ligula, nec interdum risus lacus in tortor. Suspendisse enim turpis, pellentesque vitae massa a, mattis placerat leo. Pellentesque fringilla mattis tristique. Cras id convallis ante, a tempor eros. Nullam at semper ante. Praesent eros lacus, consequat sit amet dui nec, elementum efficitur justo. In hac habitasse platea dictumst. In aliquet sapien quis risus venenatis convallis. </p>
+    </article>
+</body>
