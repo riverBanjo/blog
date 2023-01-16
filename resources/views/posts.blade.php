3380cfbea5c1d8aea6934cc1b72aec7aed5aac9c
River Fisher <riverfisher@Rivers-MBP-2.lan>
2023-01-15 20:35:27 -0500

    Episode 11: Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,16 +3,9 @@
 <link rel="stylesheet" href="/app.css">
 <script src="/app.js"></script>
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat vitae dui mollis interdum. Aliquam erat volutpat. Vivamus a interdum elit. Nullam interdum lorem diam, suscipit tempor arcu ullamcorper a. Donec vestibulum, lectus nec vehicula scelerisque, augue quam placerat ligula, nec interdum risus lacus in tortor. Suspendisse enim turpis, pellentesque vitae massa a, mattis placerat leo. Pellentesque fringilla mattis tristique. Cras id convallis ante, a tempor eros. Nullam at semper ante. Praesent eros lacus, consequat sit amet dui nec, elementum efficitur justo. In hac habitasse platea dictumst. In aliquet sapien quis risus venenatis convallis. </p>
+        <?= $post; ?>
     </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat vitae dui mollis interdum. Aliquam erat volutpat. Vivamus a interdum elit. Nullam interdum lorem diam, suscipit tempor arcu ullamcorper a. Donec vestibulum, lectus nec vehicula scelerisque, augue quam placerat ligula, nec interdum risus lacus in tortor. Suspendisse enim turpis, pellentesque vitae massa a, mattis placerat leo. Pellentesque fringilla mattis tristique. Cras id convallis ante, a tempor eros. Nullam at semper ante. Praesent eros lacus, consequat sit amet dui nec, elementum efficitur justo. In hac habitasse platea dictumst. In aliquet sapien quis risus venenatis convallis. </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat vitae dui mollis interdum. Aliquam erat volutpat. Vivamus a interdum elit. Nullam interdum lorem diam, suscipit tempor arcu ullamcorper a. Donec vestibulum, lectus nec vehicula scelerisque, augue quam placerat ligula, nec interdum risus lacus in tortor. Suspendisse enim turpis, pellentesque vitae massa a, mattis placerat leo. Pellentesque fringilla mattis tristique. Cras id convallis ante, a tempor eros. Nullam at semper ante. Praesent eros lacus, consequat sit amet dui nec, elementum efficitur justo. In hac habitasse platea dictumst. In aliquet sapien quis risus venenatis convallis. </p>
-    </article>
+    <?php endforeach; ?>
 </body>
