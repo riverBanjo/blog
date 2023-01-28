50374f89cc64514adb7b6c98b0575ff3a051b060
River Fisher <riverfisher@Rivers-MBP-2.lan>
2023-01-28 17:19:05 -0500

    Episode 12: Find a Composer Package for Post Metadata

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,10 @@
 <script src="/app.js"></script>
 <body>
     <article>
-        <?= $post; ?>
+        <h1><?= $post->title; ?></h1>
+        <div>
+            <?= $post->body; ?>
+        </div>
     </article>
     <a href="/">Go Back</a>
 </body>
