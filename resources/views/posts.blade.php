3f72ffcbdab0bbc1c9fe5f2c963c90e998b8deee
River Fisher <riverfisher@Rivers-MBP-2.lan>
2023-01-29 21:06:25 -0500

    Episode 14: Blade: The Absolute Basics

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,16 +3,17 @@
 <link rel="stylesheet" href="/app.css">
 <script src="/app.js"></script>
 <body>
-    <?php foreach ($posts as $post) : ?>
-    <article>
-        <h1>
-            <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?>
-            </a>
-        </h1>
-        <div>
-            <?= $post->excerpt; ?>
-        </div>
-    </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+        @dd($loop)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
 </body>
