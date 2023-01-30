dd1da8b8b4f6df447ea6ec50d92eb70ea0272aea
River Fisher <riverfisher@Rivers-MBP-2.lan>
2023-01-29 20:54:34 -0500

    Fix html nesting

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,8 +7,9 @@
     <article>
         <h1>
             <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?></h1>
+                <?= $post->title; ?>
             </a>
+        </h1>
         <div>
             <?= $post->excerpt; ?>
         </div>
