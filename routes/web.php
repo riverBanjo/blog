97e9cf700c1078dd650a3a8f0416aeb6f935ecb9
River Fisher <riverfisher@Rivers-MBP-2.lan>
2023-02-26 20:29:23 -0500

    Episode 25: Show All Posts Associated With a Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,11 @@ Route::get('/', function () {
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
